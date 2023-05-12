f6a516bc8e3b1b4905c1ae28808487ec415dd753
Luís Felipe <prof.luifel@gmail.com>
2023-05-12 17:55:26 -0300

    endpoist de pegar todos os games encontrados

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -8,5 +8,14 @@
 import { db } from "../database/database.connection.js";
 
 export async function gameList(req, res) {
+    try {
+        const games = await db.query(`
+            SELECT * FROM games;
+        `)
+        res.status(200).send(games.rows)
+
+    } catch(err) {
+        res.send(err.message)
+    }
 
 }
