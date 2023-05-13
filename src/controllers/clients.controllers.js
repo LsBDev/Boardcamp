146645d5d8417095a6d1afe11a6d391939afe5a0
Luís Felipe <prof.luifel@gmail.com>
2023-05-13 19:35:54 -0300

    funcão de cadastro de cliente, com a validacão birthday faltando.

--- src/controllers/clients.controllers.js
+++ src/controllers/clients.controllers.js
@@ -6,6 +6,7 @@
 //     birthday: '1992-10-25'
 // } tabela customers
 
+import dayjs from "dayjs";
 import { db } from "../database/database.connection.js";
 
 
@@ -30,6 +31,27 @@ export async function specificCLient(req, res) {
         `, [id])
         if(client.rowCount === 0) return res.sendStatus(404)
         res.status(200).send(client.rows)
+
+    } catch(err) {
+        res.send(err.message)
+    }
+}
+
+export async function insertCustomer(req, res) {
+    const {name, phone, cpf, birthday} = req.body
+
+    if(!name) return res.sendStatus(400)
+    // if(birthday !== dayjs(birthday).format("YYYY:MM:DD")) return res.status(400).send("caiu aqui!")
+    try {
+        const customerExist = await db.query(`
+            SELECT * FROM customers WHERE cpf = $1
+        `, [cpf])
+        if(customerExist.rowCount !== 0) return res.status(409).send("Cliente já possui cadastro!")
+
+        await db.query(`
+            INSERT INTO customers (name, phone, cpf, birthday) VALUES($1, $2, $3, $4);
+        `, [name, phone, cpf, birthday])
+        res.sendStatus(201)
 
     } catch(err) {
         res.send(err.message)
--- src/routes/clients.routes.js
+++ src/routes/clients.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { clientsList, specificCLient } from "../controllers/clients.controllers.js";
+import { clientsList, insertCustomer, specificCLient } from "../controllers/clients.controllers.js";
 
 
 const clientsRouter = Router()
 
 clientsRouter.get("/customers", clientsList)
 clientsRouter.get("/customers/:id", specificCLient)
+clientsRouter.post("/customers", insertCustomer)
 
 
 export default clientsRouter
