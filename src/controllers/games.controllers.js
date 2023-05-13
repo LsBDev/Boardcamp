9b22bdd5afc97e92bdf10c4f9bef77e30c79fe2b
Luís Felipe <prof.luifel@gmail.com>
2023-05-12 22:53:50 -0300

    Post /games iniciado

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -17,5 +17,24 @@ export async function gameList(req, res) {
     } catch(err) {
         res.send(err.message)
     }
+}
 
+export async function insertGame(req, res) {
+    const {name, image, stockTotal, pricePerDay} = req.body
+
+    try {
+        if(!name || stockTotal <= 0 || pricePerDay <= 0) return res.sendStatus(400)
+        const gameExist = await db.query(`
+            SELECT * FROM games WHERE name = $1;
+        `, [name])
+        if(gameExist) return res.sendStatus(409)
+
+        await db.query(`
+            INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);
+        `, [name, image, stockTotal, pricePerDay])
+        res.sendStatus(201)
+
+    } catch(err) {
+        res.send(err.message)
+    }
 }
--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { gameList } from "../controllers/games.controllers.js"
+import { gameList, insertGame } from "../controllers/games.controllers.js"
 
 const gamesRouter = Router()
 
 gamesRouter.get("/games", gameList)
+gamesRouter.post("/games", insertGame)
 // gamesRouter.get("/games", o esquema entra aqui pra validar (middleware) , gameList)
 
 
